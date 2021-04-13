421676ae982001b66ff346530f2802bf1fd3c623
Michael Pyle <mpyle101@gmail.com>
2021-04-12 22:51:54 -0600

    Store PKey for RSA

--- src/rsa.rs
+++ src/rsa.rs
@@ -21,24 +21,23 @@ pub fn from(pem: &str) -> Result<Crypter<Public>, Error> {
 }
 
 pub struct Crypter<T: HasPublic> {
-  key: Rsa<T>,
+  key: PKey<T>,
 }
 
 impl<T: HasPublic> Crypter<T> {
   pub fn new() -> Result<Crypter<Private>, Error> {
-    Ok(Crypter {
-      key: Rsa::generate(MODULOUS)?
-    })
+    let rsakey = Rsa::generate(MODULOUS)?;
+    Ok(Crypter { key: PKey::from_rsa(rsakey)? })
   }
 
   fn from(pem: &str) -> Result<Crypter<Public>, Error> {
-    let key = if pem.contains("BEGIN PUBLIC") {
+    let rsakey = if pem.contains("BEGIN PUBLIC") {
       Rsa::public_key_from_pem(pem.as_bytes())?
     } else {
       Rsa::public_key_from_pem_pkcs1(pem.as_bytes())?
     };
 
-    Ok(Crypter { key })
+    Ok(Crypter { key: PKey::from_rsa(rsakey)? })
   }
   
   pub fn to_pem(&self) -> Result<String, Error> {
@@ -46,22 +45,17 @@ impl<T: HasPublic> Crypter<T> {
     Ok(String::from_utf8(pem)?)
   }
 
-  pub fn to_pkcs1(&self) -> Result<String, Error> {
-    let pem = self.key.public_key_to_pem_pkcs1()?;
-    Ok(String::from_utf8(pem)?)
-  }
-
   pub fn encrypt(&self, data: &[u8]) -> Result<Vec<u8>, Error> {
+    let rsa = self.key.rsa()?;
     let mut buf = vec![0; self.key.size() as usize];
-    let bytes = self.key.public_encrypt(data, &mut buf, PADDING)?;
+    let bytes = rsa.public_encrypt(data, &mut buf, PADDING)?;
     buf.truncate(bytes);
 
     Ok(buf)
   }
 
   pub fn verify(&self, signature: &[u8], data: &[u8]) -> Result<bool, Error> {
-    let keypair = PKey::from_rsa(self.key.clone())?;
-    let mut verifier = Verifier::new(MessageDigest::sha256(), &keypair)?;
+    let mut verifier = Verifier::new(MessageDigest::sha256(), &self.key)?;
     verifier.update(data)?;
 
     Ok(verifier.verify(signature)?)
@@ -71,8 +65,9 @@ impl<T: HasPublic> Crypter<T> {
 impl Crypter<Private> {
   #[allow(dead_code)]
   fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>, Error> {
+    let rsa = self.key.rsa()?;
     let mut buf = vec![0; self.key.size() as usize];
-    let bytes = self.key.private_decrypt(data, &mut buf, PADDING)?;
+    let bytes = rsa.private_decrypt(data, &mut buf, PADDING)?;
     buf.truncate(bytes);
 
     Ok(buf)
@@ -81,8 +76,7 @@ impl Crypter<Private> {
   #[allow(dead_code)]
   fn sign(&self, data: &[u8]) -> Result<Vec<u8>, Error> {
     let md = MessageDigest::sha256();
-    let keypair = PKey::from_rsa(self.key.clone())?;
-    let mut signer = Signer::new(md, &keypair)?;
+    let mut signer = Signer::new(md, &self.key)?;
     signer.update(data)?;
 
     Ok(signer.sign_to_vec()?)
--- src/rsa.test.rs
+++ src/rsa.test.rs
@@ -27,19 +27,6 @@ mod tests {
   }
 
   #[test]
-  fn decrypt_by_pkcs1() {
-    let data   = b"Some secrets to encrypt";
-    let server = rsa::new().unwrap();
-    let pem    = server.to_pkcs1().unwrap();
-    let client = rsa::from(&pem).unwrap();
-    let cipher = client.encrypt(data).unwrap();
-    let plain  = server.decrypt(&cipher).unwrap();
-    
-    assert_eq!(data, &plain[..]);
-    assert_ne!(data, &cipher[..]);
-  }
-
-  #[test]
   fn signing() {
     let data    = b"Some text to verify";
     let crypter = rsa::new().unwrap();
@@ -53,17 +40,6 @@ mod tests {
     let data   = b"Some text to verify";
     let server = rsa::new().unwrap();
     let pem    = server.to_pem().unwrap();
-    let client = rsa::from(&pem).unwrap();
-    let signed = server.sign(data).unwrap();
-    
-    assert!(client.verify(&signed, data).unwrap());
-  }
-
-  #[test]
-  fn signing_by_pkcs1() {
-    let data   = b"Some text to verify";
-    let server = rsa::new().unwrap();
-    let pem    = server.to_pkcs1().unwrap();
     let client = rsa::from(&pem).unwrap();
     let signed = server.sign(data).unwrap();
     
